8517e775b5fd4301fad4717a555bd1440e3158e2
Vin Lim <vin@greenroom.com.my>
2017-12-02 12:40:55 +0800

    Add main layout structure

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @include('_layouts.meta')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+@include('_layouts.navbar-top')
+<div class="container">
+
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
