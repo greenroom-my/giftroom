f8edb73b3b769a4d4621554c7d686022f9da18c2
Jason Seah <jason@greenroom.com.my>
2017-12-02 19:56:55 +0800

    Move icons to its respective folder

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    @include('_layouts.meta')
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  @include('_layouts.meta')
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
 <div id="app">
-    <giftroom-navbar logo="false"></giftroom-navbar>
+  <giftroom-navbar logo="false"></giftroom-navbar>
 
-    <router-view></router-view>
+  <router-view></router-view>
 </div>
 
 <script src="{{ asset('js/app.js') }}"></script>
