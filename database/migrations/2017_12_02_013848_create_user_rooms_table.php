c8bdbe8a2b51d8867f83e1a5a8eb957f7b2a1035
Alex Tang <alext970620@gmail.com>
2017-12-02 10:32:26 +0800

    Add Database Migrations

---
+++ database/migrations/2017_12_02_013848_create_user_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_rooms', function (Blueprint $table) {
+            $table->increments('user_id')->unsigned();
+            $table->increments('room_id')->unsigned();
+            $table->dateTime('join_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_rooms');
+    }
+}
---
+++ database/migrations/2017_12_02_013913_create_matches_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('santa_id');
+            $table->integer('target_id');
+            $table->integer('room_id');
+            $table->timestamps();
+
+            $table->foreign('santa_id')->references('id')->on('users');
+            $table->foreign('target_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
