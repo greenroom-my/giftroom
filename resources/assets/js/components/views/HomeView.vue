d48629f604145847df756db3e36a575834cbd144
Vin Lim <vin@greenroom.com.my>
2017-12-02 16:23:18 +0800

    Add VueRouter

---
+++ resources/assets/js/components/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="home-logo text-center mt-5">
+            <span>Giftroom</span>
+        </div>
+
+        <div class="col-10 offset-1 col-sm-6 offset-sm-3">
+            <div class="small text-center">
+                <p>Giftroom helps you organise your gift exchange party including invites, matches & wishlist.</p>
+            </div>
+
+            <div class="form-group mt-5">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button class="btn btn-block btn-primary">Login</button>
+            <router-link to="/register" tag="button" class="btn btn-block btn-default">Register</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log(this.$router.currentRoute);
+        }
+    }
+</script>
---
+++ resources/assets/js/components/views/RegisterView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="col-10 offset-1 col-sm-6 offset-sm-3">
+            <h2>Register</h2>
+
+            <div class="form-group">
+                <label for="">First Name</label>
+                <input class="form-control" id="" aria-describedby="emailHelp" placeholder="Nama ape?   ">
+            </div>
+            <div class="form-group ">
+                <label for="">Last Name</label>
+                <input class="form-control" id="" aria-describedby="emailHelp" placeholder="Nama bapa?">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                       placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm again">
+            </div>
+            <button class="btn btn-block btn-primary">Register</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log(this.$router.currentRoute);
+        }
+    }
+</script>
