9b67174fb9ae37814026821d751950bb24562a5c
Ah Kiat <sk.chuah@monsteralliance.co>
2017-12-02 20:00:45 +0800

    Add API Middleware

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | Api Routes
@@ -13,24 +11,27 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::POST('user/register', '\App\Api\V1\Auth\RegisterController@register');
+Route::POST('/user/login', '\App\Api\V1\Auth\LoginController@login');
+
+
+Route::middleware(['api', 'api.auth'])->group(function () {
+
+    Route::GET('/room/{name}', '\App\Api\V1\Auth\RoomController@getRoom');
+    Route::GET('/room/room/{name}/matches');
+    Route::GET('/room/my-wish-list');
+
+    Route::POST('/room/invites');
+    Route::POST('/room');
+    Route::POST('/room/matches');
+    Route::POST('/room/my-wish-list');
+
+    Route::PATCH('/room');
+    Route::PATCH('/room/join');
+    Route::PATCH('/room/my-wish-list');
+
+    Route::DELETE('/user/{roomName}/invites');
 });
-
-Route::GET('/room/{name}','\App\Api\V1\Auth\RoomController@getRoom');
-Route::GET('/room/room/{name}/matches');
-Route::GET('/room/my-wish-list');
-
-Route::POST('user/register','\App\Api\V1\Auth\RegisterController@register');
-Route::POST('/user/login','\App\Api\V1\Auth\LoginController@login');
-//Route::POST('/user/login','App\Api\V1\Auth\LoginController@login');
-Route::POST('/room/invites');
-Route::POST('/room');
-Route::POST('/room/matches');
-Route::POST('/room/my-wish-list');
-
-Route::PATCH('/room');
-Route::PATCH('/room/join');
-Route::PATCH('/room/my-wish-list');
-
-Route::DELETE('/user/{roomName}/invites');
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
