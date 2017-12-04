1d4bff4864ee10ebf3655e568a02635d31f52e48
Vin Lim <vin@greenroom.com.my>
2017-12-05 03:36:36 +0800

    Add slideout menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,27 @@
 
 <body>
 <div id="app">
-    <giftroom-navbar :menu="navbar.menu" :logo="navbar.logo"></giftroom-navbar>
+    <Slideout menu="#menu" panel="#panel" :toggleSelectors="['.toggle-button']">
+        <nav id="menu">
+            <ul>
+                <li @click="closeMenu('/room-home')" class="btn btn-primary btn-block"><i class="fa fa-home"></i> &nbsp; Home</li>
+                <li @click="closeMenu('/wish-list')" class="btn btn-primary btn-block"><i class="fa fa-tasks"></i> &nbsp; My Wish List</li>
+                <li @click="closeMenu('/match')" class="btn btn-primary btn-block"><i class="fa fa-user"></i> &nbsp; My Match</li>
+            </ul>
 
-    <router-view></router-view>
-    <vue-up></vue-up>
+            <div class="room-switch">You are in <strong>LIMFAMILY</strong></div>
+        </nav>
+        <main id="panel">
+            <giftroom-navbar :menu="navbar.menu" :logo="navbar.logo"></giftroom-navbar>
+
+            <router-view></router-view>
+            <vue-up></vue-up>
+        </main>
+    </Slideout>
+
+
+
+
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
 
