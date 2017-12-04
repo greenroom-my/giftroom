e3a4b75164dfdefcaf1d32d8923d64f8d8c47b15
Jason Seah <jason@greenroom.com.my>
2017-12-04 20:41:22 +0800

    Add custom route binding to 'room' name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Room;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::bind('room', function ($value) {
+            return Room::where('name', $value)->first() ?? abort(404);
+        });
     }
 
     /**
