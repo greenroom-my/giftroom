0cc0b4d9cd101f4a203d972fb087f0e407666993
Vin Lim <vin@greenroom.com.my>
2017-12-05 18:40:35 +0800

    Update invite controller

--- app/Api/V1/InviteController.php
+++ app/Api/V1/InviteController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Room;
 use App\Services\InviteService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class InviteController extends Controller
